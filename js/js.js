3c08afec23881eefc0145ad48bc578ca07631a1a
saphani <52697987+saphani@users.noreply.github.com>
2021-03-31 14:21:43 +0200

    add new project on first
    ghpage

---
+++ js/js.js
@@ -0,0 +1,11 @@
+//FEATURED HOVER
+$(document).ready(function(){
+    $(".linkfeat").hover(
+      function () {
+          $(".textfeat").show(500);
+      },
+      function () {
+          $(".textfeat").hide(500);
+      }
+  );
+});
